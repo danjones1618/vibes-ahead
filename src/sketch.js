04c2d84ef0adf7d45e4724c686cab34aff38f062
danjones1618 <hello@danjones.dev>
2020-11-14 13:47:59 +0000

    Basic setup

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,60 @@
+var cols, rows;
+var scl = 20;
+var w = 1400;
+var h = 1000;
+
+var flying = 0;
+
+var terrain = [];
+
+function preload() {
+  music = loadSound('res/bensound-thejazzpiano.mp3');
+}
+
+function playMusic() {
+  //music.setLoop();
+  music.setVolume(0.1);
+  music.play();
+}
+
 function setup() {
-  createCanvas(640, 480);
+  let canvas = createCanvas(640, 480, WEBGL);
+  playMusic();
+
+  cols = w / scl;
+  rows = h / scl;
+
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; //specify a default value for now
+    }
+  }
 }
 
 function draw() {
-  background(220);
+  flying -= 0.1;
+  var yoff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
+      xoff += 0.2;
+    }
+    yoff += 0.2;
+  }
+
+  background(0);
+  translate(0, 50);
+  rotateX(PI / 3);
+  fill(200, 200, 200, 50);
+  translate(-w / 2, -h / 2);
+  for (var y = 0; y < rows - 1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
+    }
+    endShape();
+  }
 }
