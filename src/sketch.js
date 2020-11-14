f1d8a6616daff8d6b4ecbad334368a74ffbf77bf
danjones1618 <hello@danjones.dev>
2020-11-14 14:48:54 +0000

    Let's vibe with the music and the noise

--- src/sketch.js
+++ src/sketch.js
@@ -1,24 +1,31 @@
 var cols, rows;
-var scl = 20;
-var w = 1400;
-var h = 1000;
+var scl = 50;
+var w = 3500;
+var h = 1500;
 
 var flying = 0;
 
 var terrain = [];
 
 function preload() {
-  music = loadSound('res/bensound-thejazzpiano.mp3');
+  music = loadSound('res/Ocean-Shores_YOURSELF-LIVE.mp3');
 }
 
 function playMusic() {
-  //music.setLoop();
+  music.setLoop(true);
   music.setVolume(0.1);
   music.play();
 }
 
+function calcWindowSize() {
+  let width = windowWidth;
+  let height = windowHeight * 0.9 - 32 - document.getElementById("title").offsetHeight;
+  return {width, height};
+}
+
 function setup() {
-  let canvas = createCanvas(640, 480, WEBGL);
+  const {width, height} = calcWindowSize();
+  let canvas = createCanvas(width, height, WEBGL);
   playMusic();
 
   cols = w / scl;
@@ -32,8 +39,13 @@ function setup() {
   }
 }
 
+function windowResized() {
+  const {width, height} = calcWindowSize();
+  resizeCanvas(width, height);
+}
+
 function draw() {
-  flying -= 0.1;
+  flying -= 0.08;
   var yoff = flying;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
@@ -44,13 +56,15 @@ function draw() {
     yoff += 0.2;
   }
 
-  background(0);
+  background(0, 0);
   translate(0, 50);
   rotateX(PI / 3);
   fill(200, 200, 200, 50);
   translate(-w / 2, -h / 2);
   for (var y = 0; y < rows - 1; y++) {
     beginShape(TRIANGLE_STRIP);
+    stroke('#e0e0e0')
+    fill(0, 0);
     for (var x = 0; x < cols; x++) {
       vertex(x * scl, y * scl, terrain[x][y]);
       vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
